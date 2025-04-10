dc80ee084327965b4db1283b50af4df716924ef8
Niewiaro <kubuskrol@gmail.com>
2025-04-10 02:25:50 +0200

    chore: refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_phone_contacts/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,53 +26,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'My Contacts'),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool _permissionDenied = false;
-  List<Contact>? _contacts;
-
-  Future _fetchContacts() async {
-    if (!await FlutterContacts.requestPermission(readonly: true)) {
-      setState(() {
-        _permissionDenied = true;
-      });
-    } else {
-      final contacts = await FlutterContacts.getContacts();
-      setState(() {
-        _contacts = contacts;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchContacts();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body:
-          _permissionDenied
-              ? const Center(child: Text("Permission denied"))
-              : const Center(child: Text("Permission granted")),
     );
   }
 }
